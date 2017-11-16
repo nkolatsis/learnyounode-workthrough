cb89c38aebfb96d354064b80f6e82f36cc7ff2d3
Nicholas Kolatsis <nkolatsis@users.noreply.github.com>
2017-11-16 14:39:28 +0100

    LearnYouNode step 8 completed using both approaches.

---
+++ 08. HTTP Collect/program.js
@@ -0,0 +1,36 @@
+var http = require("http");
+
+http.get(process.argv[2], (response) => {
+    var collectData = []
+    response.on("data", collectData.push) // does not work
+    /*response.on("data", (data) => {
+        collectData.push(data)
+    })*/
+    response.on("error", console.error)
+    response.on("end", (end) => {
+        var dataString = collectData.reduce(function(combined, data) {
+            return combined + data
+        })
+        console.log(dataString.length)
+        console.log(dataString)
+    })
+}).on("error", console.error)
+
+/* learnyounode official solution
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+    response.pipe(bl(function (err, data) {
+    if (err) {
+        return console.error(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+    }))
+})
+
+
+
+*/
