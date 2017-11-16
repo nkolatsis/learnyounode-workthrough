6efc47c3c6649d1cfb90960d95be9b3b0da3ba07
Nicholas Kolatsis <nkolatsis@users.noreply.github.com>
2017-11-16 16:56:11 +0100

    Done for the day. Step 9 still underway.

--- 09. Juggling Async/program.js
+++ 09. Juggling Async/program.js
@@ -2,40 +2,48 @@ var http = require("http")
 var concat = require("concat-stream")
 
 var queue = []
-queue.push(getPageAsObj(process.argv[2], requestCompleted))
-queue.push(getPageAsObj(process.argv[3], requestCompleted))
-queue.push(getPageAsObj(process.argv[4], requestCompleted))
+queue.push(getPageAsObj(process.argv[2], passObjToQueue))
+queue.push(getPageAsObj(process.argv[3], passObjToQueue))
+queue.push(getPageAsObj(process.argv[4], passObjToQueue))
 
 function getPageAsObj(argv, callback) {
-    http.get(argv, (response) => { // try function(response) later
-        var collectData = []
-        response.on("data", collectData.push.bind(collectData))
+    http.get(argv, (response) => {
+        var collectedData = []
+        response.setEncoding("utf8")
+        response.on("data", (data) => {
+            collectedData.push(data)
+        })
         response.on("error", console.error)
         response.on("end", (end) => {
-            return callback(null, collectData.toString())
+            collectedData = collectedData.reduce(function(first, second) {
+                return first + second
+            })
+            return callback(null, Object(data = collectedData, completed = true))
         })
     }).on("error", console.error)
 }
 
-function requestCompleted(err, data) {
-    if (err) console.error(err)
+function passObjToQueue(err, obj) {
+    console.log(obj)
     completedCount += 1;
-    return {"data": data, completed: true}
-};
-var completedCount = 0
-while(true) {
-    if (completedCount > 2) {
-        if (queue[0]["completed"] === true &&
-            queue[1]["completed"] === true &&
-            queue[2]["completed"] === true) {
-                console.log(queue[0]["data"])
-                console.log(queue[1]["data"])
-                console.log(queue[2]["data"])
-                break
-            }
-    } else {
-        setTimeout(function(){
-            console.log(completedCount)
-        }, 500)
-    }
+    return obj;
 }
+
+var completedCount = 0
+
+setInterval(function(){
+    if (typeof(queue[0]) != undefined && queue[1] != undefined && queue[2] != undefined) {
+        console.log("got here")
+        if (queue[0]["completed"] === true) {
+            if (queue[0]["completed"] === true) {
+                if (queue[0]["completed"] === true) {
+                    console.log(queue[0]["data"])
+                    console.log(queue[1]["data"])
+                    console.log(queue[2]["data"])
+                }
+            }               
+        }
+    }
+    console.log(queue)
+    console.log(completedCount)
+}, 500)
