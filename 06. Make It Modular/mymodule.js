7e02690b3a87464f5d6802aea63bba8be70891eb
Nicholas Kolatsis <nkolatsis@users.noreply.github.com>
2017-11-16 07:21:57 +0100

    LearnYouNode Step 06. Completed

--- 06. Make It Modular/mymodule.js
+++ 06. Make It Modular/mymodule.js
@@ -1,15 +1,20 @@
-
-
-console.log(module.exports);
-
-function filterFiles(dirName, ext, fileListCallback) {
+function filterFiles(dir, ext, callback) {
     var fs = require("fs");
-    var files = fs.readdir(dirName, fileListCallback)
+    var path = require("path");
+
+    fs.readdir(dir, 'utf8', (err, list) => {
+        if (err) return callback(err);
+
+        var filterList = [];
+
+        for (let i = 0; i < list.length; i++) {
+            if (path.extname(list[i]) == "." + ext) {
+                filterList.push(list[i]);
+            }
+        }
+        return callback(null, filterList);
+    })
 }
 
-function fileListCallback(err, list) {
-    if (err) throw err;
-    return list;
-}
 
 module.exports = filterFiles;
--- 06. Make It Modular/program.js
+++ 06. Make It Modular/program.js
@@ -1,2 +1,8 @@
 var mymodule = require("./mymodule.js");
 
+mymodule(process.argv[2], process.argv[3], (err, files) => {
+    if (err) console.log(err);
+    files.forEach(function (file) {
+        console.log(file);
+    })
+})
